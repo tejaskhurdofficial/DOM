31ee7661e30912a7091b1dc16674e721a0ba77d4
tejaskhurdofficial <tejaskhurdofficial@gmail.com>
2026-02-01 11:38:52 +0530

    added code

--- 1 DOM Manupilation/1 select-element/select-element.js
+++ 1 DOM Manupilation/1 select-element/select-element.js
@@ -1,27 +1,27 @@
 let abcd = document.getElementById("abcd");
 console.log(abcd);
-console.dir(abcd);
+console.dir(abcd); // give key value pair of obj 
 
 let chacha = document.getElementsByClassName("xyz")
 console.log(chacha); // This will give you array like structure not array
 console.dir(chacha); 
 
-let chaudhary = document.querySelector("h1") // This will always select first query
+let chaudhary = document.querySelector("h1"); // This will always select first query
 console.dir(chaudhary);
 
-let sabu = document.querySelectorAll("h1") 
+let sabu = document.querySelectorAll("h1");
 console.log(sabu); // This will give you array like structure not array
 console.log(sabu);
 
-let lassai = document.querySelector("#abcd")
+let lassai = document.querySelector("#abcd");
 console.log(lassai);
 console.dir(lassai);
 
 
-let samosa = document.querySelector(".xyz")
+let samosa = document.querySelector(".xyz");
 console.log(samosa);
 console.dir(samosa);
 
-let fafda = document.querySelectorAll(".xyz")
+let fafda = document.querySelectorAll(".xyz");
 console.log(fafda);
 console.dir(fafda);
